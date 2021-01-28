cf3ef4d52a78d88291dd71c9820a2b2c169b2099
Sunny <darkowlzz@protonmail.com>
2021-01-29 02:51:45 +0530

    transform: Add DaemonSet VolumeSource transforms
    DaemonSet VolumeSource transforms for:
    - hostpath volume
    - configmap volume
    - secret volume

--- internal/transform/daemonset.go
+++ internal/transform/daemonset.go
@@ -2,8 +2,10 @@ package transform
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/darkowlzz/operator-toolkit/declarative/transform"
+	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/kustomize/kyaml/yaml"
 )
 
@@ -60,4 +62,101 @@ fieldRef:
 		return nil, err
 	}
 	return SetDaemonSetEnvVarFunc(container, key, envVarValueFrom, fieldRef), nil
+}
+
+// SetDaemonSetVolumeFunc sets a volume in a DaemonSet for the given name and
+// volume source.
+func SetDaemonSetVolumeFunc(volume string, volumeSource string, value *yaml.RNode) transform.TransformFunc {
+	return func(obj *yaml.RNode) error {
+		volumeSelector := fmt.Sprintf("[name=%s]", volume)
+		return obj.PipeE(
+			yaml.LookupCreate(yaml.ScalarNode, "spec", "template", "spec", "volumes", volumeSelector),
+			yaml.SetField(volumeSource, value),
+		)
+	}
+}
+
+// SetDaemonSetHostPathVolumeFunc sets a volume in a DaemonSet for a host path
+// volume source.
+func SetDaemonSetHostPathVolumeFunc(volume, path, pathType string) (transform.TransformFunc, error) {
+	hostPathString := fmt.Sprintf(`
+path: %s
+type: %s
+`, path, pathType)
+	hostPath, err := yaml.Parse(hostPathString)
+	if err != nil {
+		return nil, err
+	}
+	return SetDaemonSetVolumeFunc(volume, "hostPath", hostPath), nil
+}
+
+// constructVolumeSource creates a volume source.
+// nameRef is the name of the source type, nameVal is the name of the source.
+// It returns a kyaml RNode which can be used in further transformation.
+func constructVolumeSource(nameRef string, nameVal string, keyToPaths []corev1.KeyToPath) (*yaml.RNode, error) {
+	// Construct the volume source.
+	volSrcString := fmt.Sprintf(`%s: %s`, nameRef, nameVal)
+	volSrc, err := yaml.Parse(volSrcString)
+	if err != nil {
+		return nil, err
+	}
+
+	// Add items if KeyToPath are provided.
+	if len(keyToPaths) > 0 {
+		// Construct list of key path items.
+		itemsStrings := []string{}
+		for _, keyToPath := range keyToPaths {
+			itemsString := fmt.Sprintf(`
+- key: %s
+  path: %s
+`, keyToPath.Key, keyToPath.Path)
+			itemsStrings = append(itemsStrings, itemsString)
+		}
+
+		// Join the key path items to form one list.
+		itemsListString := strings.Join(itemsStrings, "\n")
+
+		// Parse the list into a RNode.
+		itemsList, err := yaml.Parse(itemsListString)
+		if err != nil {
+			return nil, err
+		}
+
+		// Add keyToPath in the volume source items.
+		err = volSrc.PipeE(
+			yaml.LookupCreate(yaml.ScalarNode),
+			yaml.SetField("items", itemsList),
+		)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return volSrc, nil
+}
+
+// SetDaemonSetConfigMapVolumeFunc sets a volume in a DaemonSet for a configmap
+// volume source.
+func SetDaemonSetConfigMapVolumeFunc(volume string, configmapName string, keyToPaths []corev1.KeyToPath) (transform.TransformFunc, error) {
+	// Construct the configmap volume source.
+	configMap, err := constructVolumeSource("name", configmapName, keyToPaths)
+	if err != nil {
+		return nil, err
+	}
+
+	// Add the configmap in the volume as configMap volume source.
+	return SetDaemonSetVolumeFunc(volume, "configMap", configMap), nil
+}
+
+// SetDaemonSetSecretVolumeFunc sets a volume in a DaemonSet for a secret
+// volume source.
+func SetDaemonSetSecretVolumeFunc(volume string, secretName string, keyToPaths []corev1.KeyToPath) (transform.TransformFunc, error) {
+	// Construct the secret volume source.
+	secret, err := constructVolumeSource("secretName", secretName, keyToPaths)
+	if err != nil {
+		return nil, err
+	}
+
+	// Add the configmap in the volume as configMap volume source.
+	return SetDaemonSetVolumeFunc(volume, "secret", secret), nil
 }
--- internal/transform/daemonset_test.go
+++ internal/transform/daemonset_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/kustomize/kyaml/yaml"
 )
 
@@ -219,6 +220,274 @@ fieldRef:
 			containerSelector := fmt.Sprintf("[name=%s]", tc.container)
 			envVarSelector := fmt.Sprintf("[name=%s]", tc.key)
 			val, err := obj.Pipe(yaml.Lookup("spec", "template", "spec", "containers", containerSelector, "env", envVarSelector, "valueFrom"))
+			assert.Nil(t, err)
+
+			str, err := val.String()
+			assert.Nil(t, err)
+			assert.Equal(t, strings.TrimSpace(tc.wantVal), strings.TrimSpace(str))
+		})
+	}
+}
+
+func TestSetDaemonSetHostPathVolumeFunc(t *testing.T) {
+	testObj, err := yaml.Parse(`
+apiVersion: apps/v1
+kind: DaemonSet
+metadata:
+  name: some-daemonset
+spec:
+  template:
+    spec:
+      containers:
+      - name: someotherapp
+        image: some-image:v1.2.3
+      volumes:
+      - name: foo
+        hostPath:
+          path: /var/lib/
+          type: Directory
+`)
+	assert.Nil(t, err)
+
+	cases := []struct {
+		name     string
+		volume   string
+		path     string
+		pathType string
+		wantVal  string
+	}{
+		{
+			name:     "new volume",
+			volume:   "somedir",
+			path:     "/var/lib/foo",
+			pathType: "Directory",
+			wantVal: `
+name: somedir
+hostPath:
+  path: /var/lib/foo
+  type: Directory`,
+		},
+		{
+			name:     "file type",
+			volume:   "somefile",
+			path:     "/xyz",
+			pathType: "File",
+			wantVal: `
+name: somefile
+hostPath:
+  path: /xyz
+  type: File`,
+		},
+		{
+			name:     "overwrite existing volume",
+			volume:   "foo",
+			path:     "/usr/local/bin/foo",
+			pathType: "File",
+			wantVal: `
+name: foo
+hostPath:
+  path: /usr/local/bin/foo
+  type: File`,
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			obj := testObj.Copy()
+
+			// Transform.
+			tf, err := SetDaemonSetHostPathVolumeFunc(tc.volume, tc.path, tc.pathType)
+			assert.Nil(t, err)
+			err = tf(obj)
+			assert.Nil(t, err)
+
+			volumeSelector := fmt.Sprintf("[name=%s]", tc.volume)
+			val, err := obj.Pipe(yaml.Lookup("spec", "template", "spec", "volumes", volumeSelector))
+			assert.Nil(t, err)
+
+			str, err := val.String()
+			assert.Nil(t, err)
+			assert.Equal(t, strings.TrimSpace(tc.wantVal), strings.TrimSpace(str))
+		})
+	}
+}
+
+func TestSetDaemonSetConfigMapVolumeFunc(t *testing.T) {
+	testObj, err := yaml.Parse(`
+apiVersion: apps/v1
+kind: DaemonSet
+metadata:
+  name: some-daemonset
+spec:
+  template:
+    spec:
+      containers:
+      - name: someotherapp
+        image: some-image:v1.2.3
+      volumes:
+      - name: foo
+        configMap:
+          name: app-config
+`)
+	assert.Nil(t, err)
+
+	cases := []struct {
+		name          string
+		volume        string
+		configMapName string
+		keyToPaths    []corev1.KeyToPath
+		wantVal       string
+	}{
+		{
+			name:          "add new volume",
+			volume:        "config-vol",
+			configMapName: "my-config",
+			wantVal: `
+name: config-vol
+configMap:
+  name: my-config`,
+		},
+		{
+			name:          "overwrite existing volume",
+			volume:        "foo",
+			configMapName: "foo-config",
+			keyToPaths: []corev1.KeyToPath{
+				{Key: "somekey", Path: "/somepath/"},
+			},
+			wantVal: `
+name: foo
+configMap:
+  name: foo-config
+  items:
+    - key: somekey
+      path: /somepath/`,
+		},
+		{
+			name:          "with items",
+			volume:        "some-config-vol",
+			configMapName: "some-config",
+			keyToPaths: []corev1.KeyToPath{
+				{Key: "key1", Path: "/some/path1"},
+				{Key: "key2", Path: "/some/path2"},
+			},
+			wantVal: `
+name: some-config-vol
+configMap:
+  name: some-config
+  items:
+    - key: key1
+      path: /some/path1
+    - key: key2
+      path: /some/path2`,
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			obj := testObj.Copy()
+
+			// Transform.
+			tf, err := SetDaemonSetConfigMapVolumeFunc(tc.volume, tc.configMapName, tc.keyToPaths)
+			assert.Nil(t, err)
+			err = tf(obj)
+			assert.Nil(t, err)
+
+			volumeSelector := fmt.Sprintf("[name=%s]", tc.volume)
+			val, err := obj.Pipe(yaml.Lookup("spec", "template", "spec", "volumes", volumeSelector))
+			assert.Nil(t, err)
+
+			str, err := val.String()
+			assert.Nil(t, err)
+			assert.Equal(t, strings.TrimSpace(tc.wantVal), strings.TrimSpace(str))
+		})
+	}
+}
+
+func TestSetDaemonSetSecretVolumeFunc(t *testing.T) {
+	testObj, err := yaml.Parse(`
+apiVersion: apps/v1
+kind: DaemonSet
+metadata:
+  name: some-daemonset
+spec:
+  template:
+    spec:
+      containers:
+      - name: someotherapp
+        image: some-image:v1.2.3
+      volumes:
+      - name: foo
+        secret:
+          secretName: mysecret
+`)
+	assert.Nil(t, err)
+
+	cases := []struct {
+		name       string
+		volume     string
+		secretName string
+		keyToPaths []corev1.KeyToPath
+		wantVal    string
+	}{
+		{
+			name:       "add new volume",
+			volume:     "secret-vol",
+			secretName: "my-secret",
+			wantVal: `
+name: secret-vol
+secret:
+  secretName: my-secret`,
+		},
+		{
+			name:       "overwrite existing volume",
+			volume:     "foo",
+			secretName: "foo-secret",
+			keyToPaths: []corev1.KeyToPath{
+				{Key: "somekey", Path: "/somepath/"},
+			},
+			wantVal: `
+name: foo
+secret:
+  secretName: foo-secret
+  items:
+    - key: somekey
+      path: /somepath/`,
+		},
+		{
+			name:       "with items",
+			volume:     "some-secret-vol",
+			secretName: "some-secret",
+			keyToPaths: []corev1.KeyToPath{
+				{Key: "key1", Path: "/some/path1"},
+				{Key: "key2", Path: "/some/path2"},
+			},
+			wantVal: `
+name: some-secret-vol
+secret:
+  secretName: some-secret
+  items:
+    - key: key1
+      path: /some/path1
+    - key: key2
+      path: /some/path2`,
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			obj := testObj.Copy()
+
+			// Transform.
+			tf, err := SetDaemonSetSecretVolumeFunc(tc.volume, tc.secretName, tc.keyToPaths)
+			assert.Nil(t, err)
+			err = tf(obj)
+			assert.Nil(t, err)
+
+			volumeSelector := fmt.Sprintf("[name=%s]", tc.volume)
+			val, err := obj.Pipe(yaml.Lookup("spec", "template", "spec", "volumes", volumeSelector))
 			assert.Nil(t, err)
 
 			str, err := val.String()
