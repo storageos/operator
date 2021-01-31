9f8fdd7a471f9b0b70190f2c78aef8ddce218a1d
Sunny <darkowlzz@protonmail.com>
2021-01-31 21:28:11 +0530

    Add Service transforms

---
+++ internal/transform/service.go
@@ -0,0 +1,76 @@
+package transform
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/darkowlzz/operator-toolkit/declarative/transform"
+	corev1 "k8s.io/api/core/v1"
+	kyaml "sigs.k8s.io/kustomize/kyaml/yaml"
+)
+
+// SetServiceNameFunc sets the name of a given Service.
+func SetServiceNameFunc(name string) transform.TransformFunc {
+	return func(obj *kyaml.RNode) error {
+		return obj.PipeE(
+			kyaml.LookupCreate(kyaml.ScalarNode, "metadata"),
+			kyaml.SetField("name", kyaml.NewScalarRNode(name)),
+		)
+	}
+}
+
+// SetDefaultServicePortNameFunc sets the default port name of the Service.
+func SetDefaultServicePortNameFunc(name string) transform.TransformFunc {
+	return func(obj *kyaml.RNode) error {
+		portsObj, err := obj.Pipe(
+			kyaml.LookupCreate(kyaml.ScalarNode, "spec", "ports"),
+		)
+		if err != nil {
+			return err
+		}
+
+		// TODO: Find proper error returned when elements is empty and handle
+		// the error appropriately by creating a default port entry.
+		ports, err := portsObj.Elements()
+		if err != nil {
+			return err
+		}
+
+		// Set the first port.
+		return ports[0].PipeE(
+			kyaml.SetField("name", kyaml.NewScalarRNode(name)),
+		)
+	}
+}
+
+// SetServiceTypeFunc sets the type of a Service.
+func SetServiceTypeFunc(serviceType corev1.ServiceType) transform.TransformFunc {
+	return func(obj *kyaml.RNode) error {
+		return obj.PipeE(
+			kyaml.LookupCreate(kyaml.ScalarNode, "spec"),
+			kyaml.SetField("type", kyaml.NewScalarRNode(string(serviceType))),
+		)
+	}
+}
+
+// SetServiceInternalPortFunc sets the internal port of a Service port.
+func SetServiceInternalPortFunc(portName string, port int) transform.TransformFunc {
+	return func(obj *kyaml.RNode) error {
+		portSelector := fmt.Sprintf("[name=%s]", portName)
+		return obj.PipeE(
+			kyaml.LookupCreate(kyaml.ScalarNode, "spec", "ports", portSelector),
+			kyaml.SetField("port", kyaml.NewScalarRNode(strconv.Itoa(port))),
+		)
+	}
+}
+
+// SetServiceExternalPortFunc sets the external port of a Service port.
+func SetServiceExternalPortFunc(portName string, port int) transform.TransformFunc {
+	return func(obj *kyaml.RNode) error {
+		portSelector := fmt.Sprintf("[name=%s]", portName)
+		return obj.PipeE(
+			kyaml.LookupCreate(kyaml.ScalarNode, "spec", "ports", portSelector),
+			kyaml.SetField("targetPort", kyaml.NewScalarRNode(strconv.Itoa(port))),
+		)
+	}
+}
---
+++ internal/transform/service_test.go
@@ -0,0 +1,225 @@
+package transform
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	kyaml "sigs.k8s.io/kustomize/kyaml/yaml"
+)
+
+func TestSetServiceNameFunc(t *testing.T) {
+	testObj, err := kyaml.Parse(`
+apiVersion: v1
+kind: Service
+metadata:
+  name: storageos
+spec:
+  ports:
+  - name: storageos
+    port: 5705
+    protocol: TCP
+    targetPort: 5705
+  sessionAffinity: None
+  type: ClusterIP
+`)
+	assert.Nil(t, err)
+
+	wantName := "foo-svc"
+
+	// Transform.
+	tf := SetServiceNameFunc(wantName)
+	err = tf(testObj)
+	assert.Nil(t, err)
+
+	// Query and check the value.
+	val, err := testObj.Pipe(kyaml.Lookup("metadata", "name"))
+	assert.Nil(t, err)
+	str, err := val.String()
+	assert.Nil(t, err)
+	assert.Equal(t, wantName, strings.TrimSpace(str))
+}
+
+func TestSetDefaultServicePortNameFunc(t *testing.T) {
+	testObj, err := kyaml.Parse(`
+apiVersion: v1
+kind: Service
+metadata:
+  name: storageos
+spec:
+  ports:
+  - name: storageos
+    port: 5705
+    protocol: TCP
+    targetPort: 5705
+  type: ClusterIP
+`)
+	assert.Nil(t, err)
+
+	wantName := "foo-svc"
+
+	// Transform.
+	tf := SetDefaultServicePortNameFunc(wantName)
+	err = tf(testObj)
+	assert.Nil(t, err)
+
+	// Query and check the value.
+	portSelector := fmt.Sprintf("[name=%s]", wantName)
+	portName, err := testObj.Pipe(kyaml.Lookup("spec", "ports", portSelector, "name"))
+	assert.Nil(t, err)
+	str, err := portName.String()
+	assert.Nil(t, err)
+	assert.Equal(t, wantName, strings.TrimSpace(str))
+}
+
+func TestSetServiceTypeFunc(t *testing.T) {
+	testObj, err := kyaml.Parse(`
+apiVersion: v1
+kind: Service
+metadata:
+  name: storageos
+spec:
+  ports:
+  - name: storageos
+    port: 5705
+    protocol: TCP
+    targetPort: 5705
+  type: ClusterIP
+`)
+	assert.Nil(t, err)
+
+	wantType := corev1.ServiceTypeNodePort
+
+	// Transform.
+	tf := SetServiceTypeFunc(wantType)
+	err = tf(testObj)
+	assert.Nil(t, err)
+
+	// Query and check the value.
+	typeName, err := testObj.Pipe(kyaml.Lookup("spec", "type"))
+	assert.Nil(t, err)
+	str, err := typeName.String()
+	assert.Nil(t, err)
+	assert.Equal(t, string(wantType), strings.TrimSpace(str))
+}
+
+func TestSetServiceInternalPortFunc(t *testing.T) {
+	testObj, err := kyaml.Parse(`
+apiVersion: v1
+kind: Service
+metadata:
+  name: appsvc
+spec:
+  ports:
+  - name: svc1
+    port: 7777
+    protocol: TCP
+    targetPort: 9999
+  - name: svc2
+    port: 2222
+    protocol: TCP
+    targetPort: 6666
+  type: ClusterIP
+`)
+	assert.Nil(t, err)
+
+	cases := []struct {
+		name     string
+		portName string
+		port     int
+	}{
+		{
+			name:     "set first port",
+			portName: "svc1",
+			port:     1111,
+		},
+		{
+			name:     "set second port",
+			portName: "svc2",
+			port:     5555,
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			// Make a copy of the object.
+			obj := testObj.Copy()
+
+			// Transform.
+			tf := SetServiceInternalPortFunc(tc.portName, tc.port)
+			err = tf(obj)
+			assert.Nil(t, err)
+
+			// Query and check the value.
+			portSelector := fmt.Sprintf("[name=%s]", tc.portName)
+			port, err := obj.Pipe(kyaml.Lookup("spec", "ports", portSelector, "port"))
+			assert.Nil(t, err)
+			str, err := port.String()
+			assert.Nil(t, err)
+			assert.Equal(t, strconv.Itoa(tc.port), strings.TrimSpace(str))
+		})
+	}
+}
+
+func TestSetServiceExternalPortFunc(t *testing.T) {
+	testObj, err := kyaml.Parse(`
+apiVersion: v1
+kind: Service
+metadata:
+  name: appsvc
+spec:
+  ports:
+  - name: svc1
+    port: 7777
+    protocol: TCP
+    targetPort: 9999
+  - name: svc2
+    port: 2222
+    protocol: TCP
+    targetPort: 6666
+  type: ClusterIP
+`)
+	assert.Nil(t, err)
+
+	cases := []struct {
+		name     string
+		portName string
+		port     int
+	}{
+		{
+			name:     "set first port",
+			portName: "svc1",
+			port:     1111,
+		},
+		{
+			name:     "set second port",
+			portName: "svc2",
+			port:     5555,
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			// Make a copy of the object.
+			obj := testObj.Copy()
+
+			// Transform.
+			tf := SetServiceExternalPortFunc(tc.portName, tc.port)
+			err = tf(obj)
+			assert.Nil(t, err)
+
+			// Query and check the value.
+			portSelector := fmt.Sprintf("[name=%s]", tc.portName)
+			port, err := obj.Pipe(kyaml.Lookup("spec", "ports", portSelector, "targetPort"))
+			assert.Nil(t, err)
+			str, err := port.String()
+			assert.Nil(t, err)
+			assert.Equal(t, strconv.Itoa(tc.port), strings.TrimSpace(str))
+		})
+	}
+}
